7a196703e8c5a7fb312b4a37ebeb4fd5075bf4c6
spencech <spencech@gmail.com>
2022-07-03 09:47:20 -0400

    updating readme

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,6 @@
 import * as _ from 'underscore';
 import * as $ from 'jquery';
 
-export function trace(...parameters: any[]) {
-  if (!getparams().debug) return;
-
-  for (let i = 0, count = parameters.length; i < count; i++) {
-    // tslint:disable-next-line
-    console.log(parameters[i]);
-  }
-}
-
-export function falsy(e: any): boolean {
-  if (typeof e === 'string' && trim(e).match(/^false$/i)) return true;
-  if (typeof e === 'string') return _.isEmpty(e.replace(/\s+/gim, ''));
-  if (typeof e === 'boolean') return e === false;
-  if (!isNaN(parseInt(e, 10))) return e === 0;
-  return _.isEmpty(e);
-}
-
-export function truthy(e:any): boolean {
-  return !falsy(e);
-}
-
-export function empty(e: any): boolean {
-  return falsy(e);
-}
-
-export function isset(e: any): boolean {
-  return truthy(e);
-}
-
-export function clone(obj: any): any {
-  if (obj === null || obj === undefined) return obj;
-  return JSON.parse(JSON.stringify(obj));
-}
-
-export function delay(method: () => void, ms: number = 0): number {
-  return window.setTimeout(method, ms);
-}
-
-
 
 export function alphabetize(...parameters: any[]): any {
   if (typeof parameters[0] === 'string') {
@@ -62,35 +23,24 @@ export function alphabetize(...parameters: any[]): any {
   }
 }
 
-export function trim(e: string): string {
-  if (_.isEmpty(e)) return e;
-  return e.replace(/^\s+/, '').replace(/\s+$/, '');
+export function clone(obj: any): any {
+  if (obj === null || obj === undefined) return obj;
+  return JSON.parse(JSON.stringify(obj));
 }
 
-export function snakecase(e: string): string {
-  if (_.isEmpty(e)) return e;
-  return e.toLowerCase().replace(/\s+/gim, '_').replace(/-/g,"_").replace(/_+/g,"_");
+export function dateStrings(date?: Date) {
+  date = date || new Date();
+  return {
+    year: date.getFullYear(),
+    month: (date.getMonth() + 1).toString().padStart(2, '0'),
+    date: date.getDate().toString().padStart(2, '0'),
+    time: `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`,
+    seconds: date.getSeconds().toString().padStart(2, '0'),
+  };
 }
 
-export function kebab(e: string): string {
-  if (_.isEmpty(e)) return e;
-  return e.toLowerCase().replace(/\s+/gim, '-').replace(/_/g, '-').replace(/-+/g, '-');
-}
-
-export function getparams(requestedProperty?: string): any {
-  const vars = {} as any;
-  const parts = window.location.href.replace(/[?&#]+([^=&]+)=([^&]*)/gi, ((m: any, key: string, value: any) => {
-    vars[key] = value;
-  }) as any);
-
-  for (const prop in vars) {
-    if (vars[prop].toLowerCase() === 'true') vars[prop] = true;
-    else if (vars[prop].toLowerCase() === 'false') vars[prop] = false;
-    else if (!isNaN(parseFloat(vars[prop])) && !vars[prop].match(/[^0-9]+/gim)) vars[prop] = parseFloat(vars[prop]);
-  }
-
-  if (requestedProperty) return vars[requestedProperty];
-  return vars;
+export function delay(method: () => void, ms: number = 0): number {
+  return window.setTimeout(method, ms);
 }
 
 export function download(content: string, name: string, extension: string = "csv") {
@@ -106,6 +56,22 @@ export function download(content: string, name: string, extension: string = "csv
     a.click();
     document.body.removeChild(a);
   }
+}
+
+export function empty(e: any): boolean {
+  return falsy(e);
+}
+
+export function falsy(e: any): boolean {
+  if (typeof e === 'string' && trim(e).match(/^false$/i)) return true;
+  if (typeof e === 'string') return _.isEmpty(e.replace(/\s+/gim, ''));
+  if (typeof e === 'boolean') return e === false;
+  if (!isNaN(parseInt(e, 10))) return e === 0;
+  return _.isEmpty(e);
+}
+
+export function focus(selector: string): number {
+  return delay(() => $(selector).focus());
 }
 
 export function getLargestRemainder(values: number[], desiredSum: number) {
@@ -135,37 +101,29 @@ export function getLargestRemainder(values: number[], desiredSum: number) {
   return valueParts.sort((a, b) => a.originalIndex - b.originalIndex).map((p) => p.integer);
 }
 
-export function validateEmail(email: string | null): RegExpMatchArray | null {
-  if (!email) return null;
+export function getparams(requestedProperty?: string): any {
+  const vars = {} as any;
+  const parts = window.location.href.replace(/[?&#]+([^=&]+)=([^&]*)/gi, ((m: any, key: string, value: any) => {
+    vars[key] = value;
+  }) as any);
 
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    );
+  for (const prop in vars) {
+    if (vars[prop].toLowerCase() === 'true') vars[prop] = true;
+    else if (vars[prop].toLowerCase() === 'false') vars[prop] = false;
+    else if (!isNaN(parseFloat(vars[prop])) && !vars[prop].match(/[^0-9]+/gim)) vars[prop] = parseFloat(vars[prop]);
+  }
+
+  if (requestedProperty) return vars[requestedProperty];
+  return vars;
 }
 
-export function timestamp(date?: Date, includeTime: boolean = true): string {
-  const info = dateStrings(date);
-  const time = `${info.time}:${info.seconds}`;
-  return `${info.year}-${info.month}-${info.date}${includeTime ? time : ''}`;
+export function isset(e: any): boolean {
+  return truthy(e);
 }
 
-export function dateStrings(date?: Date) {
-  date = date || new Date();
-  return {
-    year: date.getFullYear(),
-    month: (date.getMonth() + 1).toString().padStart(2, '0'),
-    date: date.getDate().toString().padStart(2, '0'),
-    time: `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`,
-    seconds: date.getSeconds().toString().padStart(2, '0'),
-  };
-}
-
-export function tick(returnValue?: any): Promise<any> {
-  return new Promise((resolve: (response: any) => void, reject: (response: any) => void) => {
-    delay(() => resolve(returnValue));
-  });
+export function kebab(e: string): string {
+  if (_.isEmpty(e)) return e;
+  return e.toLowerCase().replace(/\s+/gim, '-').replace(/_/g, '-').replace(/-+/g, '-');
 }
 
 export function replaceItem(array: any[], item: any, key: string = 'id', position: string = 'current'): any {
@@ -191,6 +149,51 @@ export function replaceItem(array: any[], item: any, key: string = 'id', positio
   return item;
 }
 
+export function snakecase(e: string): string {
+  if (_.isEmpty(e)) return e;
+  return e.toLowerCase().replace(/\s+/gim, '_').replace(/-/g,"_").replace(/_+/g,"_");
+}
+
+export function tick(returnValue?: any): Promise<any> {
+  return new Promise((resolve: (response: any) => void, reject: (response: any) => void) => {
+    delay(() => resolve(returnValue));
+  });
+}
+
+export function timestamp(date?: Date, includeTime: boolean = true): string {
+  const info = dateStrings(date);
+  const time = `${info.time}:${info.seconds}`;
+  return `${info.year}-${info.month}-${info.date}${includeTime ? time : ''}`;
+}
+
+export function trace(...parameters: any[]) {
+  if (!getparams().debug) return;
+
+  for (let i = 0, count = parameters.length; i < count; i++) {
+    // tslint:disable-next-line
+    console.log(parameters[i]);
+  }
+}
+
+export function trim(e: string): string {
+  if (_.isEmpty(e)) return e;
+  return e.replace(/^\s+/, '').replace(/\s+$/, '');
+}
+
+export function truthy(e:any): boolean {
+  return !falsy(e);
+}
+
+export function validateEmail(email: string | null): RegExpMatchArray | null {
+  if (!email) return null;
+
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+}
+
 export function viewport(el: HTMLElement, percentVisible: number = 100): boolean {
   const rect = el.getBoundingClientRect();
   const windowHeight = window.innerHeight || document.documentElement.clientHeight;
@@ -199,8 +202,4 @@ export function viewport(el: HTMLElement, percentVisible: number = 100): boolean
     Math.floor(100 - ((rect.top >= 0 ? 0 : rect.top) / +-(rect.height / 1)) * 100) < percentVisible ||
     Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentVisible
   );
-}
-
-export function focus(selector: string): number {
-  return delay(() => $(selector).focus());
 }
